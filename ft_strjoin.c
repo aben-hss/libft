2e4913f52199470ce34e370d37ce88c6a17ab20f
Ahmad Ben Hssain <aben-hss@e1r6p1.1337.ma>
2023-12-03 19:49:04 +0100

    libft_125

---
+++ ft_strjoin.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aben-hss <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/27 03:07:32 by aben-hss          #+#    #+#             */
+/*   Updated: 2023/11/27 03:07:33 by aben-hss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static char	*ft_strcat(char *dst, const char *src)
+{
+	char		*d;
+	const char	*s;
+
+	d = dst;
+	s = src;
+	while (*d != '\0')
+		d++;
+	while (*s != '\0')
+	{
+		*d = *s;
+		d++;
+		s++;
+	}
+	*d = '\0';
+	return (dst);
+}
+
+static char	*ft_strcpy(char *dest, const char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		++i;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*new_str;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	new_str = (char *)malloc(s1_len + s2_len + 1);
+	if (new_str == NULL)
+		return (NULL);
+	ft_strcpy(new_str, s1);
+	ft_strcat(new_str, s2);
+	return (new_str);
+}
